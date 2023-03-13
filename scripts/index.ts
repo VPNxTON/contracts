e168afa2e0068ddcad596b1afd5fc2ebd6a572a2
murluki <timofeli.work@gmail.com>
2023-03-13 17:12:29 +0100

    nft item and nft collection contracts

---
+++ scripts/index.ts
@@ -0,0 +1,15 @@
+import {Address, TonClient} from "ton"
+
+async function main () {
+
+  const wallet = Address.parse('USER_WALLET_ADDRESS'); //adress of the user
+  const collection = Address.parse('COLLECTION_ADDRESS'); //where do we add them 
+
+  const client = new TonClient({
+    endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+    apiKey: 'YOUR_API_KEY',
+  })
+
+}
+
+main()
